6ac532dce870d6c7f8b8b72fe7cc17aa0d3cb110
Vanyahilevich <40438281+Vanyahilevich@users.noreply.github.com>
2022-01-23 22:20:00 +0300

    feat: project2version

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,21 @@
-console.log("100 - все окей кроме валидности, на линки и векторную графику ругается ")
+console.log("ругается на ссылки и не придерживался perfectPixel ")
+
+
+
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.header__nav')
+const ul = document.querySelector(".header__nav")
+burger.addEventListener("click", () => {
+
+	burger.classList.toggle("burger-active")
+	nav.classList.toggle("header__nav__active")
+
+})
+
+ul.addEventListener('click', (event) => {
+	// burger.classList.remove("burger-active")
+	burger.classList.toggle("burger-active")
+	nav.classList.toggle("header__nav__active")
+
+console.log(event.target)
+})
